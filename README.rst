fbdbfd5c062f55c1dad334edd2274eda8e7c96cf
bninja <andrew@poundpay.com>
2014-06-10 05:17:00 -0700

    travis

---
+++ README.rst
@@ -0,0 +1,6 @@
+=====
+nacha
+=====
+
+.. image:: https://travis-ci.org/bninja/nacha.png
+   :target: https://travis-ci.org/bninja/nacha
--- tests/__init__.py
+++ tests/__init__.py
@@ -1,5 +1,9 @@
 import os
-import unittest
+
+try:
+    import unittest2 as unittest
+except ImportError:
+    import unittest
 
 
 class TestCase(unittest.TestCase):
